f41e341559eacdcf64fcbd126d232b0f53f47419
Chris Lane <chrisl@cube19.com>
2019-12-04 18:13:19 +0000

    Implement day 2 part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -1,29 +1,60 @@
 use std::process::exit;
 
 pub fn part1(input: &str) -> String {
-    let mut pos = 0;
-    let mut program: Vec<u32> = input
+    let program: Vec<u32> = input
         .split(",")
         .filter_map(|s| s.parse().ok())
         .collect::<Vec<_>>();
 
-    loop {
-        let op = program[pos];
+    run_program(&program, program[1], program[2])
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(",")
+}
 
+pub fn part2(input: &str) -> String {
+    let required_output = 19690720;
+    let max_value = 99;
+    let program: Vec<u32> = input
+        .split(",")
+        .filter_map(|s| s.parse().ok())
+        .collect::<Vec<_>>();
+
+    for a in 0..max_value + 1 {
+        for b in 0..max_value + 1 {
+            if run_program(&program, a, b)[0] == required_output {
+                return (100 * a + b).to_string();
+            }
+        }
+    }
+
+    "No solution! :(".to_string()
+}
+
+fn run_program(program: &Vec<u32>, noun: u32, verb: u32) -> Vec<u32> {
+    let mut pos = 0;
+    let mut result: Vec<u32> = program.clone();
+
+    result[1] = noun;
+    result[2] = verb;
+
+    loop {
+        let op = result[pos];
 
         match op {
             1 | 2 => {
-                let val1_pos = program[pos + 1] as usize;
-                let val2_pos = program[pos + 2] as usize;
-                let result_pos = program[pos + 3] as usize;
-                let val1 = program[val1_pos];
-                let val2 = program[val2_pos];
-                program[result_pos] = if op == 1 {
+                let val1_pos = result[pos + 1] as usize;
+                let val2_pos = result[pos + 2] as usize;
+                let result_pos = result[pos + 3] as usize;
+                let val1 = result[val1_pos];
+                let val2 = result[val2_pos];
+                result[result_pos] = if op == 1 {
                     val1 + val2
                 } else {
                     val1 * val2
                 }
-            },
+            }
             99 => break,
             _ => {
                 eprintln!("Unexpected operator in input");
@@ -34,15 +65,7 @@ pub fn part1(input: &str) -> String {
         pos += 4;
     }
 
-    program
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>()
-        .join(",")
-}
-
-pub fn part2(input: &str) -> String {
-    input.to_string()
+    result
 }
 
 #[cfg(test)]
@@ -55,12 +78,5 @@ mod tests {
         assert_eq!(part1("2,3,0,3,99"), "2,3,0,6,99");
         assert_eq!(part1("2,4,4,5,99,0"), "2,4,4,5,99,9801");
         assert_eq!(part1("1,1,1,4,99,5,6,0,99"), "30,1,1,4,2,5,6,0,99");
-    }
-
-    #[test]
-    fn part2_test() {
-        assert_eq!(part2("14"), "2");
-        assert_eq!(part2("1969"), "966");
-        assert_eq!(part2("100756"), "50346");
     }
 }
